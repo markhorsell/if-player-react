da3d4f964efeac6fa365c93e16cad193873ce57f
markhorsell <mark.horsell@gmail.com>
2019-09-24 21:08:59 +0100

    .

--- src/components/WorldMap.tsx
+++ src/components/WorldMap.tsx
@@ -1,17 +1,11 @@
 import React, { useEffect, useState, useRef } from "react";
-
 import { debounce } from "lodash";
-
 import styled from "styled-components/macro";
 
 const isLocal = window.location.href.substr(7, 9) === "localhost";
-
-let WorldMapDiv: any;
-
 const canvasPath = isLocal ? "url('../assets/theshivers/images/game_bg.jpg')" : "url('https://raw.githubusercontent.com/markhorsell/if-player-react/gh-pages/assets/theshivers/images/game_bg.jpg')";
 
-
-WorldMapDiv = styled.div`
+const WorldMapDiv = styled.div`
     > canvas {
       border-radius: 10px;
       display: inline;
