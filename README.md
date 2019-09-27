7bb3e849b880e1bb71466bd91787539b4643adb9
markhorsell <mark.horsell@gmail.com>
2019-09-27 23:06:10 +0100

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-Interactive fiction player
-
-Reference project Interactive Fiction game engine, attempting to stay up to date with current React, Redux and TypeScript best practice.
-
---
+++ precache-manifest.126fb657fa2db7bd17c0480e0eab6554.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1f7713511682338bf9228baae90dd3",
+    "url": "/if-player-react/index.html"
+  },
+  {
+    "revision": "30cec826ccbeb6ceb6b9",
+    "url": "/if-player-react/static/js/2.32b35a5a.chunk.js"
+  },
+  {
+    "revision": "e8185d6ecb5a752bab65",
+    "url": "/if-player-react/static/js/main.7510af73.chunk.js"
+  },
+  {
+    "revision": "f22e112d1ccfbd629223",
+    "url": "/if-player-react/static/js/runtime-main.cfb99375.js"
+  }
+]);
