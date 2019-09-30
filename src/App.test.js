98df87976e6781a80425163d163255dbbc128279
markhorsell <mark.horsell@gmail.com>
2019-09-30 19:25:03 +0100

    test with --coverage

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-//import { act } from "react-test-renderer";
 import {act} from 'react-dom/test-utils';
 
 it('renders without crashing', async () => {
-  
   await act(async () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
