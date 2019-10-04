0e3af05d983428fea802ddd062764e2e855dbd0f
markhorsell <mark.horsell@gmail.com>
2019-10-04 21:23:44 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/if-player-react/precache-manifest.9b68b05091dcd09dacd5a06ce376de48.js"
+  "/if-player-react/precache-manifest.d99989fb4fbcf443f8ce4051ce234e99.js"
 );
 
 self.addEventListener('message', (event) => {
