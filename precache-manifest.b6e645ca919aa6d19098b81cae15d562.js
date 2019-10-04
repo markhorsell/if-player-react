1f56f9a6629befad68be767f30c45923c535e83c
markhorsell <mark.horsell@gmail.com>
2019-10-04 19:22:51 +0100

    Updates

---
+++ precache-manifest.b6e645ca919aa6d19098b81cae15d562.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f38fb761ac16b2f4144bab3baba3769f",
+    "url": "/if-player-react/index.html"
+  },
+  {
+    "revision": "04bc2133422678c135da",
+    "url": "/if-player-react/static/js/2.2c84d365.chunk.js"
+  },
+  {
+    "revision": "7c967128a740324813fd",
+    "url": "/if-player-react/static/js/3.3f96a422.chunk.js"
+  },
+  {
+    "revision": "d52fc6b50ce474e15da4",
+    "url": "/if-player-react/static/js/4.6c732a91.chunk.js"
+  },
+  {
+    "revision": "2cbf047d43c4ec094ac7",
+    "url": "/if-player-react/static/js/main.78386824.chunk.js"
+  },
+  {
+    "revision": "a821699683a7a4ec9453",
+    "url": "/if-player-react/static/js/runtime-main.2b39f67a.js"
+  }
+]);
