539bde4ebd8f3d50520f262afd6d4d8fb9e515ea
markhorsell <mark.horsell@gmail.com>
2019-10-04 19:18:42 +0100

    basename the router

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App: React.SFC = () => {
           <ThemeProvider theme={theme}>
             <>
               <GlobalStyles />
-              <Router>
+              <Router basename={process.env.PUBLIC_URL}>
                 <Suspense fallback={<div>Loading...</div>}>
                   <AppContent />
                 </Suspense>
